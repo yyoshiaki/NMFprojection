f61b65802907d50a416dd76860c6bb5de7165be0
yyoshiaki <y.yasumizu@gmail.com>
2022-04-26 16:08:02 +0900

    alpha

--- NMFprojection.py
+++ NMFprojection.py
@@ -1,4 +1,5 @@
 import argparse
+import warnings
 
 import pandas as pd
 import numpy as np
@@ -9,35 +10,35 @@ from sklearn.decomposition import non_negative_factorization
 seed = 0
 
 
-def NMFprojection(f_input, f_fixedW, f_outputprefix, normalized=False)
-    X = pd.read_csv(f_input, delim_whitespace=True) # gene x cell or samples
-    fixed_W = pd.read_csv(f_fixedW, delim_whitespace=True) # gene' x components
-    
+def NMFprojection(X, fixed_W, normalized=False):
     # normalize X
     if normalized == False:
         X = (X * 10**4) / X.sum()
         X = np.log1p(X+1)
-    
+
+    if (normalized == False) & (X.max().max() > 20):
+        warnings.warn("input X looks not normalized though normalized flag was passed")
+    if (normalized == True) & (X.max().max() < 20):
+        warnings.warn("input X looks normalized though normalized flag was not passed")
+
     # intersect genes
-    genes = list(set(X.index) & set(gene.index))
-    X = X.loc[gene]
-    fixed_W = fixed_W.loc[gene]
+    genes = list(set(X.index) & set(fixed_W.index))
+    X = X.loc[genes]
+    fixed_W = fixed_W.loc[genes]
     fixed_H = fixed_W.T
     
-    W, _, n_iter = non_negative_factorization(X, n_components=2, init='custom', 
-                                              random_state=seed, update_H=False, H=fixed_H)
+    W, _, n_iter = non_negative_factorization(np.array(X.T, dtype = 'float64'), n_components=fixed_H.shape[0], 
+                                          init='custom', random_state=seed, update_H=False, 
+                                          H=np.array(fixed_H, dtype = 'float64'))
     H = W.T
     df_H = pd.DataFrame(H, columns=X.columns, index=fixed_W.columns)
-    df_RSS = pd.DataFrame(((W.dot(H) - X)**2).sum(axis=0), columns=['Error'], index=X.columns)
+    df_RSS = pd.DataFrame(np.sqrt(((fixed_W.dot(df_H) - X) ** 2).sum(axis=0)) / X.shape[0], 
+                            columns=['Error'], index=X.columns)
     
     print("stats of RSS")
-    print(df_error.describe())
+    print(df_RSS.describe())
     
-    H.to_csv('{}_projection.csv'.format(f_outputprefix))
-    df_RSS.to_csv('{}_RSS.csv'.format(f_outputprefix))
-    
-    return df_H, df_error
-    
+    return X, df_H, df_RSS
     
 
 if __name__ == "__main__":
@@ -51,5 +52,12 @@ if __name__ == "__main__":
     parser.add_argument('--normalized', action='store_true', 
                         help='if normalized and log transformed, specify this flag.')
     args = parser.parse_args()
-    
-    NMFprojection(args.input, args.fixedW, args.outputprefix, normalized=args.normalized)
+
+    X = pd.read_csv(args.input, index_col=0, delim_whitespace=True) # gene x cell or samples
+    fixed_W = pd.read_csv(args.fixedW, index_col=0) # gene' x components
+    f_outputprefix = args.outputprefix
+
+    _, df_H, df_RSS = NMFprojection(X, fixed_W, normalized=args.normalized)
+
+    df_H.to_csv('{}_projection.csv'.format(f_outputprefix))
+    df_RSS.to_csv('{}_RSS.csv'.format(f_outputprefix))
--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # NMFprojection
 NMFprojection
+
+## Tested environment
+
+- Python==3.8.10
+- numpy==1.20.3
+- scipy==1.7.1
+- pandas==1.3.3
+- scikit-learn==0.24.2
+
+## Usage
+
+```
+usage: NMFprojection.py [-h] [--outputprefix OUTPUTPREFIX] [--normalized]
+                        input fixedW
+
+NMFprojection
+
+positional arguments:
+  input                 input csv/tsv of gene expressions. UMI, scaledTPM, TPM
+                        can be used. Row: genes, Columns: samples.
+  fixedW                input csv/tsv of precomputed W. Row: genes, Columns:
+                        components.
+
+optional arguments:
+  -h, --help            show this help message and exit
+  --outputprefix OUTPUTPREFIX
+                        output prefix. default=NMF
+  --normalized          if normalized and log transformed, specify this flag.
+```
+
+example
+```
+python NMFprojection.py --outputprefix test/STR1.5_Fr1.2.3.5.6 test/STR1.5_Fr1.2.3.5.6_scaledTPM.tsv data/NMF.W.CD4T.csv.gz
+```
+
+## Deposited W
+
+- CD4T cell (pan-autoimmune peripheral CD4T, yasumizu et al., unpublished) : `data/NMF.W.CD4T.csv.gz`
